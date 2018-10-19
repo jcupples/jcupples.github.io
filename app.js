59281ce3b9da35f726e84bbcd2a50de435cb9797
Jonathan Cupples <jonathan.c.cupples@gmail.com>
2018-10-19 13:24:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 (function($)
  {
-    var s=skrollr.init({render:function(data){}})})(jQuery);$(document).ready(function()
-        {$(".main").onepage_scroll(
-            {sectionContainer:"section",easing:"ease",animationTime:900,pagination:true,updateURL:false
+    var s=skrollr.init({render:function(data){}})})(jQuery);
+    $(document).ready(function()
+        {
+        $(".main").onepage_scroll(
+            {
+                sectionContainer:"section",easing:"ease",animationTime:900,pagination:true,updateURL:false
             })
         });
 function init()
     {
         window.setTimeout(function()
-                {
-                    start()
-                },2e3)
+            {
+                start()
+            },
+        2e3)
     }
 function start()
     {
         $("body").removeClass("loading").addClass("loaded")
-    }$(window).load
+    }
+    $(window).load
     
 (function()
     {
